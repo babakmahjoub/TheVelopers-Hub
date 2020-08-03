879eb0fcd0c53d8f9bfa356d2765078dfbf5c2e8
babak mahjoub <62224494+babakmahjoub@users.noreply.github.com>
2020-08-03 14:28:29 +1000

    Add Add & Delete Education

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,17 +8,6 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-//Test Middleware
-router.get('/', auth, async (req, res) => {
-  try {
-    const user = await User.findById(req.user.id).select('-password');
-    res.json(user);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
 //Login user
 router.post(
   '/',
