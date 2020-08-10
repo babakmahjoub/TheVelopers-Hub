2dc3cbdab582a2c68e752ba7fc17ce1785a934d9
babak mahjoub <62224494+babakmahjoub@users.noreply.github.com>
2020-08-10 11:37:53 +1000

    Setup Protected Route

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = ({ component: Component, ...rest }) => {
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
